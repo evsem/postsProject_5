1446099de05dc7207757ad69daeb87b794a3ba81
evsem <frukwork@gmail.com>
2022-02-25 12:34:17 +0300

    beforeSelectAndSearch

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import ButtonGrey from '../../UI/ButtonGrey/ButtonGrey'
 import classes from './Item.module.css'
 
-const Item = () => {
+const Item = ({ post, removePost, number }) => {
   return (
     <div className={classes.wrapper}>
       <div className={classes.container}>
-        <h3 className={classes.container_titlePost}>JS</h3>
-        <p className={classes.container_infoAboutPost}>Programming language</p>
+        <h3 className={classes.container_titlePost}>
+          {number}. {post.title}
+        </h3>
+        <p className={classes.container_infoAboutPost}>{post.body}</p>
       </div>
+      <ButtonGrey onClick={() => removePost(post)}>Delete</ButtonGrey>
     </div>
   )
 }
--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Item from '../Item/Item'
 import classes from './List.module.css'
 
-const List = () => {
-  return <div className={classes.wrapper}></div>
+const List = ({ posts, removePost }) => {
+  return (
+    <div className={classes.wrapper}>
+      {posts.map((post, idx) => (
+        <Item
+          key={post.id}
+          number={1 + idx}
+          post={post}
+          removePost={removePost}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default List
